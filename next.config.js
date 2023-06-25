063fc5c69247071889bf9af1c74f84a0714d8f11
ku1987 <kei.usami1987@gmail.com>
2023-06-25 18:10:13 +0900

    Set up env vars

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+require("dotenv").config();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    openAiApiKey: process.env.OPENAI_API_KEY,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
